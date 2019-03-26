a53d3ff3cd95dc5e44a92bf5b7afb1afc0aa78d6
Westborn <george@westborn.com.au>
2019-03-26 15:42:07 +1100

    :sparkles:Part 1

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
